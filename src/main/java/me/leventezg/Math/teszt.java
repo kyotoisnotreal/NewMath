8d42ff8d135959b944e325d886efbad3e3b62d65
kyotoisnotreal <leventezggamer@gmail.com>
2026-06-29 19:55:55 +0200

    Fixing packages

--- src/main/java/me/leventezg/Math/teszt.java
+++ src/main/java/me/leventezg/Math/teszt.java
@@ -2,7 +2,7 @@ package me.leventezg.Math;
 
 public class teszt {
     public static void main(String[] args) {
-        System.out.println(me.leventezg.MathPlus.MathPlus.isPrime(1187));
+        System.out.println(MathPlus.isPrime(1187));
 
 
 
